b28a623a9be597da8928e1d32c3a12b9c9d6510b
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-03 02:04:10 -0700

    021

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,6 +2,9 @@ import React, { useState } from "react"
 import { images } from "../images"
 import { motion, AnimatePresence } from "framer-motion"
 import { wrap } from "@popmotion/popcorn"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+//import { faArrowLeft } from '@fortawesome/react-fontawesome'
 
 function Slider() {
   const [[page, direction], setPage] = useState([0, 0])
@@ -25,13 +28,29 @@ function Slider() {
             exit={{ opacity: 0 }}
             transition={{
               x: { type: "spring", stiffness: 300, damping: 300 },
-              opacity: { duration: 1 },
+              opacity: { duration: 0.6 },
             }}
             drag="x"
             dragConstraints={{ left: 0, right: 0 }}
             dragElastic={1}
           />
         </AnimatePresence>
+        <motion.button
+          whileHover={{ scale: 1.2, transition: { duration: 0.5 } }}
+          whileTop={{ scale: 0.9 }}
+          className="next"
+          onClick={() => paginate(1)}
+        >
+          <FontAwesomeIcon icon={faArrowRight} size="2x" />
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.2, transition: { duration: 0.5 } }}
+          whileTop={{ scale: 0.9 }}
+          className="prev"
+          onClick={() => paginate(-1)}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+        </motion.button>
       </div>
     </div>
   )
