c8f98792e7c19f04f6338faf0b95316c6f912ddb
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-03 01:47:49 -0700

    014

---
+++ src/images.js
@@ -0,0 +1,7 @@
+export const images = [
+  "https://images.unsplash.com/photo-1545214766-04116e13521c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80",
+  "https://images.unsplash.com/photo-1565369728672-523bc7e59df2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80",
+  "https://images.unsplash.com/photo-1544946853-801555e27f3d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+  "https://images.unsplash.com/photo-1593867237029-ac67c5c3eb88?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1453721574860-f128dc7c920d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+]
